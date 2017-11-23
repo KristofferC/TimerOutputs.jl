9178e1d5cc2bd39f5dd48c6c2ef18a8b3be072bb
Fredrik Ekre <fredrik.ekre@chalmers.se>
2017-11-23 09:55:28 +0100

    compat for strwidth -> textwidth (#30)

--- src/TimerOutputs.jl
+++ src/TimerOutputs.jl
@@ -6,6 +6,7 @@ import Base: show, time_ns, gc_bytes
 export TimerOutput, @timeit, reset_timer!, print_timer, timeit
 
 using Crayons
+using Compat.textwidth
 
 include("TimerOutput.jl")
 include("show.jl")
