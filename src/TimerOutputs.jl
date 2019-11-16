6c0d8eda12b3e3e3dba9e669060cd03497eb3b98
Fredrik Ekre <ekrefredrik@gmail.com>
2019-11-16 23:45:39 +0100

    Fix call to Base.gc_bytes (fixes #67). Remote __precompile__. Set version to 0.5.3.

--- src/TimerOutputs.jl
+++ src/TimerOutputs.jl
@@ -1,9 +1,18 @@
-__precompile__()
-
 module TimerOutputs
 
-import Base: show, time_ns, gc_bytes
+import Base: show, time_ns
 export TimerOutput, @timeit, @timeit_debug, reset_timer!, print_timer, timeit
+
+# https://github.com/JuliaLang/julia/pull/33717
+if VERSION < v"1.4.0-DEV.475"
+    gc_bytes() = Base.gc_bytes()
+else
+    function gc_bytes()
+        b = Ref{Int64}(0)
+        Base.gc_bytes(b)
+        return b[]
+    end
+end
 
 using Printf
 
