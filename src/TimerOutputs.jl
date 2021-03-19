4a8a18d29094d083ebdd7e13a61964db2172b07b
Juan Ignacio Polanco <jipolanc@gmail.com>
2021-03-19 08:16:21 +0100

    Export AbstractTimerOutput

--- src/TimerOutputs.jl
+++ src/TimerOutputs.jl
@@ -5,7 +5,7 @@ using ExprTools
 import Base: show, time_ns
 export TimerOutput, @timeit, @timeit_debug, reset_timer!, print_timer, timeit,
                     enable_timer!, disable_timer!, @notimeit
-export NoTimerOutput
+export AbtractTimerOutput, NoTimerOutput
 
 # https://github.com/JuliaLang/julia/pull/33717
 if VERSION < v"1.4.0-DEV.475"
