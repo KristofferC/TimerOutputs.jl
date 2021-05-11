ec58e16feaaed70a8233328e7e7e2f9cc2b67c9b
Tim Besard <tim@juliacomputing.com>
2021-05-11 11:41:11 +0200

    Use ExprTools, reuse _timer_expr, rework timeit_debug func def.

--- src/TimerOutputs.jl
+++ src/TimerOutputs.jl
@@ -1,5 +1,7 @@
 module TimerOutputs
 
+using ExprTools
+
 import Base: show, time_ns
 export TimerOutput, @timeit, @timeit_debug, reset_timer!, print_timer, timeit,
                     enable_timer!, disable_timer!, @notimeit
