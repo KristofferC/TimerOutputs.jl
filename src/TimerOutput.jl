a29287ccfecd74d8962f375cfa166938b684a463
KristofferC <kristoffer.carlsson@juliacomputing.com>
2023-05-16 14:21:16 +0200

    remove line numbers from the macro created code

--- src/TimerOutput.jl
+++ src/TimerOutput.jl
@@ -246,15 +246,15 @@ function _timer_expr(m::Module, is_debug::Bool, to::Union{Symbol, Expr, TimerOut
     end
 
     if is_debug
-        return quote
+        return Base.remove_linenums!(quote
             if $m.timeit_debug_enabled()
                 $timeit_block
             else
                 $ex
             end
-        end
+        end)
     else
-        return timeit_block
+        return Base.remove_linenums!(timeit_block)
     end
 end
 
