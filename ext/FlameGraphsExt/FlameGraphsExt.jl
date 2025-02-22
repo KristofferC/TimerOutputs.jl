abcb26096120de0ad2f6357753799c04a3761ab1
Ian Butterworth <i.r.butterworth@gmail.com>
2025-02-22 15:52:13 -0500

    improve flamegraph generation

--- ext/FlameGraphsExt/FlameGraphsExt.jl
+++ ext/FlameGraphsExt/FlameGraphsExt.jl
@@ -1,39 +1,60 @@
 module FlameGraphsExt
 
 using TimerOutputs
-using FlameGraphs: FlameGraphs
+using TimerOutputs: prettytime
+using FlameGraphs: FlameGraphs, NodeData
 using FlameGraphs.LeftChildRightSiblingTrees: Node, addchild
 using Base.StackTraces: StackFrame
 
+"""
+    flamegraph(to::TimerOutput; crop_root = false)
 
-function FlameGraphs.flamegraph(to::TimerOutput)
+Create a flamegraph from a TimerOutput. The flamegraph will show the time spent in each
+function, with the width of each box proportional to the time spent in that function.
+Use `crop_root = true` to crop the root node to the first and last child nodes.
+"""
+function FlameGraphs.flamegraph(to::TimerOutput; crop_root = false)
     # Skip the very top-level node, which contains no useful data
-    node_data = _flamegraph_frame(to, to.start_data.time; toplevel=true)
+    very_start = crop_root ? min_start_time(to) : to.start_data.time
+    node_data = _flamegraph_frame(to, very_start; toplevel=true, crop_root)
     root = Node(node_data)
-    return _to_flamegraph(to, root, to.start_data.time)
+    return _to_flamegraph(to, root, very_start)
 end
 
 ## internals
 
+function min_start_time(to::TimerOutput)
+    return minimum(child.start_data.time for child in values(to.inner_timers))
+end
+
 function max_end_time(to::TimerOutput)
-    return maximum(child.start_data.time + child.accumulated_data.time for child in values(to.inner_timers))
+    self_end = to.start_data.time + to.accumulated_data.time
+    if isempty(to.inner_timers)
+        return self_end
+    end
+    # Compute max end time considering both direct end time and all inner timers
+    return max(self_end, maximum(max_end_time(child) for child in values(to.inner_timers)))
 end
 
 # Make a flat frame for this TimerOutput
-function _flamegraph_frame(to::TimerOutput, start_ns; toplevel = false)
+function _flamegraph_frame(to::TimerOutput, start_ns; toplevel = false, crop_root = true)
     # TODO: Use a better conversion to a StackFrame so this contains the right kind of data
-    tt = Symbol(to.name)
-    sf = StackFrame(tt, Symbol("none"), 0, nothing, false, false, UInt64(0x0))
+    tt = Symbol(string(to.name, " (", strip(prettytime(to.accumulated_data.time)), ")"))
+    # Set the pointer to ensure the sf is unique
+    sf = StackFrame(tt, Symbol("none"), 0, nothing, false, false, Base.objectid(to))
     status = 0x0  # "default" status -- See FlameGraphs.jl
-    start = to.start_data.time - start_ns
     # TODO: is this supposed to be inclusive or exclusive?
     if toplevel
         # The root frame covers the total time being measured, so start when the first node
         # was created, and stop when the last node was finished.
-        range = Int(start) : Int(start + (max_end_time(to) - start_ns))
+        _start = crop_root ? min_start_time(to) : to.start_data.time
+        _end = max_end_time(to)
+        range = (Int(_start) : Int(_end)) .- start_ns
     else
         #range = Int(start) : Int(start + TimerOutputs.tottime(to))
-        range = Int(start) : Int(start + to.accumulated_data.time)
+        _start = to.start_data.time
+        _end = to.start_data.time + to.accumulated_data.time
+        range = (Int(_start) : Int(_end)) .- start_ns
     end
     return FlameGraphs.NodeData(sf, status, range)
 end
